078531c559f766066fe29a6877a848f9b7c5644e
zht98 <70802119+zht98@users.noreply.github.com>
2025-04-25 21:49:56 +0800

    Update script.py

--- script.py
+++ script.py
@@ -1,7 +1,7 @@
 import requests
 
 def fetch_and_filter():
-    url = 'https://media-jxnc-fy-person01.jx6oss.ctyunxs.cn/PERSONCLOUD/9f92d85e-13e6-4fb1-97b4-8e402293d285.nzk?response-content-disposition=attachment%3Bfilename%3D%22gggg.nzk%22%3Bfilename*%3DUTF-8%27%27gggg.nzk&x-amz-CLIENTNETWORK=UNKNOWN&x-amz-CLOUDTYPEIN=PERSON&x-amz-CLIENTTYPEIN=PC&Signature=0p%2B0dLb4CmTP6agP95uCP2odbWI%3D&AWSAccessKeyId=0Lg7dAq3ZfHvePP8DKEU&x-amz-userLevel=100&Expires=1745585805&x-amz-limitrate=51200&x-amz-FSIZE=607298&x-amz-UID=672532495&x-amz-UFID=624201191216557947'
+    url = 'http://mdxgh.tpddns.cn:9999/new/mdzb.txt'
     
     # 获取文件内容
     response = requests.get(url)
